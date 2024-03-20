4b643122334fba6e7bff2a2a961ce869f456af81
James Hendry <jh455@st-andrews.ac.uk>
2024-03-20 14:44:38 +0000

    Pattern match for some tests

--- Code/TestOperations.hs
+++ Code/TestOperations.hs
@@ -18,6 +18,11 @@ instance Arbitrary NumberValue where
         fmap (FloatVal) arbitrary
         ])
 
+-- Convert a Value (NumberValue) to a Float
+numToFloat :: Value -> Float
+numToFloat v = case v of
+                IntVal i -> fromIntegral i
+                FloatVal f -> f
 
 -- Constructor for an expression that takes two arguments such as Add / Sub
 type TwoArgOperation = Expr -> Expr -> Expr
@@ -28,19 +33,21 @@ type TwoIntOperation = Int -> Int -> Int
 type TwoFloatOperation = Float -> Float -> Float
 type FloatAndIntOperation = Float -> Int -> Float
 
+
+
 {-
  - Helper function to verify that a given Expr that takes two int arguments
  - always suceeds when given two ints and produces the same result as the
  - simple operation
 -}
-testEval2IntArithmetic :: TwoArgOperation -> TwoIntOperation -> Int -> Int -> Bool
-testEval2IntArithmetic opExpr fResult x y = evalBasic expr == Right expected
+checkEval2Int :: TwoArgOperation -> TwoIntOperation -> Int -> Int -> Property
+checkEval2Int opExpr fResult x y = evalBasic expr === Right expected
     where
         expr = opExpr (Val (IntVal x)) (Val (IntVal y))
         expected = IntVal (fResult x y)
 
-testEval2FloatArithmetic :: TwoArgOperation -> TwoFloatOperation -> Float -> Float -> Bool
-testEval2FloatArithmetic opExpr fResult x y = evalBasic expr == Right expected
+checkEval2Float :: TwoArgOperation -> TwoFloatOperation -> Float -> Float -> Property
+checkEval2Float opExpr fResult x y = evalBasic expr === Right expected
     where
         expr = opExpr (Val (FloatVal x)) (Val (FloatVal y))
         expected = FloatVal (fResult x y)
@@ -55,44 +62,19 @@ testEval2MixedComArithmetic opExpr fResult i f = evalBasic expr1 === Right expec
         expected = FloatVal (fResult f (fromIntegral i))
 
 {- Addition a + b -}
-prop_testEvalAddInts :: Int -> Int -> Bool
-prop_testEvalAddInts = testEval2IntArithmetic Add (+)
-
-prop_testEvalAddFloats :: Float -> Float -> Bool
-prop_testEvalAddFloats = testEval2FloatArithmetic Add (+)
-
-prop_testEvalAddMixed :: Int -> Float -> Property
-prop_testEvalAddMixed = testEval2MixedComArithmetic Add (+)
+prop_testEvalAdd :: NumberValue -> NumberValue -> Property
+prop_testEvalAdd (NumberValue (IntVal a)) (NumberValue (IntVal b)) = checkEval2Int Add (+) a b
+prop_testEvalAdd (NumberValue a) (NumberValue b) = checkEval2Float Add (+) (numToFloat a) (numToFloat b)
 
 {- Subtraction a - b -}
-prop_testEvalSubInts :: Int -> Int -> Bool
-prop_testEvalSubInts = testEval2IntArithmetic Sub (-)
-
-prop_testEvalSubFloats :: Float -> Float -> Bool
-prop_testEvalSubFloats = testEval2FloatArithmetic Sub (-)
-
-prop_testEvalSubIntFloat :: Int -> Float -> Property
-prop_testEvalSubIntFloat i f = evalBasic expr === Right expected
-    where
-        expr = Sub (Val (IntVal i)) (Val (FloatVal f))
-        expected = FloatVal $ (fromIntegral i) - f
-
-prop_testEvalSubFloatInt :: Float -> Int -> Property
-prop_testEvalSubFloatInt f i = evalBasic expr === Right expected
-    where
-        expr = Sub (Val (IntVal i)) (Val (FloatVal f))
-        expected = FloatVal $ (fromIntegral i) - f
+prop_testEvalSub :: NumberValue -> NumberValue -> Property
+prop_testEvalSub (NumberValue (IntVal a)) (NumberValue (IntVal b)) = checkEval2Int Sub (-) a b
+prop_testEvalSub (NumberValue a) (NumberValue b) = checkEval2Float Sub (-) (numToFloat a) (numToFloat b)
 
 {- Multiplication a * b -}
-prop_testEvalMulInts :: Int -> Int -> Bool
-prop_testEvalMulInts = testEval2IntArithmetic Mul (*)
-
-prop_testEvalMulFloats :: Float -> Float -> Bool
-prop_testEvalMulFloats = testEval2FloatArithmetic Mul (*)
-
-prop_testEvalMulMixed :: Int -> Float -> Property
-prop_testEvalMulMixed = testEval2MixedComArithmetic Mul (*)
-
+prop_testEvalMul :: NumberValue -> NumberValue -> Property
+prop_testEvalMul (NumberValue (IntVal a)) (NumberValue (IntVal b)) = checkEval2Int Mul (*) a b
+prop_testEvalMul (NumberValue a) (NumberValue b) = checkEval2Float Mul (*) (numToFloat a) (numToFloat b)
 
 
 zero_i_expr :: Expr
@@ -105,19 +87,25 @@ ensureMathError (Left (MathError _)) = property True
 ensureMathError other = counterexample ("Expected MathError, got: " ++ show other) False
 
 {- Division a / b -}
--- Test integer division by first multiplying to ensure that it will divide
+prop_testDiv :: NumberValue -> NumberValue -> Property
+prop_testDiv (NumberValue a) (NumberValue (IntVal 0)) = ensureMathError $ evalBasic expr
+    where expr = Div (Val a) zero_i_expr
+
+prop_testDiv (NumberValue a) (NumberValue (FloatVal 0.0)) = ensureMathError $ evalBasic expr
+    where expr = Div (Val a) zero_f_expr
+
+prop_testDiv (NumberValue (IntVal a)) (NumberValue (IntVal b)) =
+    a `mod` b /= 0 ==> checkEval2Float Div (/) (fromIntegral a) (fromIntegral b)
+
+prop_testDiv (NumberValue a) (NumberValue b) = checkEval2Float Div (/) (numToFloat a) (numToFloat b)
+
+-- Check integer division when they divide perfectly
 prop_testIntDiv :: Int -> Int -> Property
-prop_testIntDiv a b = a /= 0 ==> testEval2IntArithmetic Div (\_ _ -> b) ab a
-    where ab = a * b
-
-prop_testEvalDiv0 :: NumberValue -> Property
-prop_testEvalDiv0 (NumberValue v) = ensureMathError $ evalBasic expr
-    where expr = Div (Val v) zero_i_expr
-
+prop_testIntDiv a b = b /= 0 ==> checkEval2Int Div (div) (a*b) b
 
 {- Modulo Tests -}
 prop_testEvalMod :: Int -> Int -> Property
-prop_testEvalMod a b = b /= 0 ==> testEval2IntArithmetic Mod mod a b
+prop_testEvalMod a b = b /= 0 ==> checkEval2Int Mod mod a b
 
 prop_testEvalMod0 :: NumberValue -> Property
 prop_testEvalMod0 (NumberValue v) = ensureMathError $ evalBasic expr
@@ -126,7 +114,7 @@ prop_testEvalMod0 (NumberValue v) = ensureMathError $ evalBasic expr
 
 {- Power AKA a^x tests -}
 prop_testEvalPowIntNon0 :: Int -> Int -> Property
-prop_testEvalPowIntNon0 a x = a /= 0 ==> testEval2IntArithmetic Pow (^) a x
+prop_testEvalPowIntNon0 a x = a /= 0 ==> checkEval2Int Pow (^) a x
 
 -- Skip sqrt-like as complex for negative a
 prop_testEvalPowFloatReg :: Positive Float -> Float -> Property
