473e27e7acf3d276025b0deb7b12eee35cf68c5e
st275 <st275@st-andrews.ac.uk>
2024-03-22 14:52:53 +0000

    Cleaned up errors, added cases for BoolVals & comparison for int & float in Eval.hs

--- Code/Eval.hs
+++ Code/Eval.hs
@@ -15,7 +15,7 @@ eval :: Vars -> -- Variable name to value mapping
 eval vars fs (Val x) = Right x -- for values, just give the value directly
 eval vars fs (Var name) = case value name vars of -- for values, just give the value directly
                                Right x -> Right x
-                               Left _ -> Left $ ValueError("value does not exist")
+                               Left _ -> Left $ ValueError("Value does not exist")
                                 
 eval vars fs (Add x y) = do
     xVal <- eval vars fs x
@@ -25,7 +25,7 @@ eval vars fs (Add x y) = do
         (FloatVal fltA, FloatVal fltB) -> Right (FloatVal (fltA + fltB))
         (IntVal int, FloatVal flt) -> Right (FloatVal (fromIntegral int + flt))
         (FloatVal flt, IntVal int) -> Right (FloatVal (flt + fromIntegral int))
-        _ -> Left $ ValueError "flawed addition operation"
+        _ -> Left $ ValueError "Cannot add given values"
 
 eval vars fs (Sub x y) = do
     xVal <- eval vars fs x
@@ -35,7 +35,7 @@ eval vars fs (Sub x y) = do
         (FloatVal fltA, FloatVal fltB) -> Right (FloatVal (fltA - fltB))
         (IntVal int, FloatVal flt) -> Right (FloatVal (fromIntegral int - flt))
         (FloatVal flt, IntVal int) -> Right (FloatVal (flt - fromIntegral int))
-        _ -> Left $ ValueError "flawed subtraction operation"
+        _ -> Left $ ValueError "Cannot subtract given values"
 
 eval vars fs (Mul x y) = do
     xVal <- eval vars fs x
@@ -45,44 +45,44 @@ eval vars fs (Mul x y) = do
         (FloatVal fltA, FloatVal fltB) -> Right (FloatVal (fltA * fltB))
         (IntVal int, FloatVal flt) -> Right (FloatVal (fromIntegral int * flt))
         (FloatVal flt, IntVal int) -> Right (FloatVal (flt * fromIntegral int))
-        _ ->  Left $ ValueError "flawed multiplication operation"
+        _ ->  Left $ ValueError "Cannot multiply given values"
 
 eval vars fs (Div x y) = do
     xVal <- eval vars fs x
     yVal <- eval vars fs y
     case (xVal, yVal) of
-        (_, IntVal 0) -> Left $ MathError "program does not support imaginary numbers"
-        (_, FloatVal 0.0) -> Left $ MathError "program does not support imaginary numbers"
+        (_, IntVal 0) -> Left $ MathError "Program does not support imaginary numbers"
+        (_, FloatVal 0.0) -> Left $ MathError "Program does not support imaginary numbers"
         (IntVal intA, IntVal intB) -> if (intA `mod` intB == 0)
                                             then Right (IntVal (intA `div` intB))
                                             else Right (FloatVal (fromIntegral intA / fromIntegral intB))
         (FloatVal fltA, FloatVal fltB) -> Right (FloatVal (fltA / fltB))
         (IntVal int, FloatVal flt) -> Right (FloatVal (fromIntegral int / flt))
         (FloatVal flt, IntVal int) -> Right (FloatVal (flt / fromIntegral int))
-        _ -> Left $ ValueError "flawed division operation"
+        _ -> Left $ ValueError "Cannot divide given values"
 
 eval vars fs (Concat a b) = case (eval vars fs a, eval vars fs b) of
                                 (Right (StrVal x), Right (StrVal y)) -> Right ((StrVal (x ++ y)))
-                                _ -> Left $ ValueError "cannot concatenate"
+                                _ -> Left $ ValueError "Cannot concatenate given values"
 
 eval vars fs (Abs a) = do
     val <- eval vars fs a
     case val of
         IntVal int -> Right (IntVal (abs int))
         FloatVal flt -> Right (FloatVal (abs flt))
-        _ -> Left $ ValueError "flawed absolute value operation"
+        _ -> Left $ ValueError "Cannot calculate absolute value of given value"
 
 eval vars fs (Mod x y) = do
     dividend <- eval vars fs x
     divisor <- eval vars fs y
     case (dividend, divisor) of
-        (_, IntVal 0) -> Left $ MathError "cannot do modulo with 0"
-        (_, FloatVal 0.0) -> Left $ MathError "cannot do modulo with 0"
+        (_, IntVal 0) -> Left $ MathError "Cannot do modulo with 0"
+        (_, FloatVal 0.0) -> Left $ MathError "Cannot do modulo with 0"
         (IntVal intA, IntVal intB) -> return (IntVal (intA `mod` intB))
         (FloatVal fltA, FloatVal fltB) -> return (FloatVal (snd (divMod' fltA fltB)))
         (IntVal int, FloatVal flt) -> return (FloatVal (snd (divMod' (fromIntegral int) flt)))
         (FloatVal flt, IntVal int) -> return (FloatVal (snd (divMod' flt (fromIntegral int))))
-        _ -> Left $ ValueError "flawed modulo operation"
+        _ -> Left $ ValueError "Cannot caclulate modulo of given values"
 
 eval vars fs (Pow x y) = do
     base <- eval vars fs x
@@ -90,35 +90,40 @@ eval vars fs (Pow x y) = do
     case (base, exp) of
         (IntVal 0, _) -> Right (IntVal 0)
         (FloatVal 0.0, _) -> Right (FloatVal 0.0)
-        (IntVal intA, IntVal intB) | intA == 0 && intB < 0 -> Left $ MathError "division by 0 is undefined"
+        (IntVal intA, IntVal intB) | intA == 0 && intB < 0 -> Left $ MathError "Division by 0 is undefined"
                                    | intB < 0              -> Right $ FloatVal ((fromIntegral intA) ** (fromIntegral intB)) -- negative exponent == float
                                    | otherwise             -> Right (IntVal (intA ^ intB))
         (FloatVal fltA, FloatVal fltB) -> Right (FloatVal (fltA ** fltB))
         (IntVal int, FloatVal flt) -> Right (FloatVal (fromIntegral int ** flt))
         (FloatVal flt, IntVal int) -> Right (FloatVal (flt ** fromIntegral int))
+        _ -> Left $ ValueError "Cannot calculate power of given values"
 
 eval vars fs (Sqrt a) = do
     val <- eval vars fs a
     case val of
-        IntVal int | int < 0 -> Left $ MathError "negatives not supported for this operation"
+        IntVal int | int < 0 -> Left $ MathError "Negative are not supported for this operation"
                    | otherwise -> Right (FloatVal (sqrt $ fromIntegral int))
-        FloatVal flt | flt < 0.0 ->  Left $ MathError "negatives not supported for this operation"
+        FloatVal flt | flt < 0.0 ->  Left $ MathError "Negative are not supported for this operation"
                      | otherwise -> Right (FloatVal (sqrt flt))
-        _ -> Left $ MathError "flawed square root operation"
+        _ -> Left $ MathError "Cannot calculate square root of given value"
 
 eval vars fs (ToString e) = do
     case eval vars fs e of
         Right (StrVal s) -> Right $ StrVal s
         Right (IntVal i) -> Right $ StrVal (show i)
         Right (FloatVal f) -> Right $ StrVal (show f)
-        Left _ -> Left (ValueError "cannot convert to string")
+        Right (BoolVal b) -> Right $ StrVal (show b)
+        Left err -> Left $ err -- Passes up error raised inside of ToString call
 
 eval vars fs (ToInt e) = do
     value <- eval vars fs e
     case value of
         StrVal s -> case readMaybe s of 
                         Just x -> Right (IntVal x)
-                        Nothing -> Left (ValueError "cannot convert to int")
+                        Nothing -> Left (ValueError "Cannot convert given value to int")
+        BoolVal b -> case b of
+                        True -> Right (IntVal 1) -- 1 for true
+                        False -> Right (IntVal 0) -- 0 for false
         IntVal i -> Right (IntVal i)
         FloatVal f -> Right (IntVal (truncate f))
 
@@ -127,13 +132,14 @@ eval vars fs (ToFloat e) = do
     case value of
         StrVal s -> case readMaybe s of
                         Just x -> Right (FloatVal x)
-                        Nothing -> Left (ValueError "cannot convert to float")
+                        Nothing -> Left (ValueError "Cannot convert given value to float")
         IntVal i -> Right (FloatVal (fromIntegral i))
         FloatVal f -> Right (FloatVal f)
+        _ -> Left $ ValueError "Cannot convert given value to float"
 
 eval vars fs (CallUserFunc name args) = do
     (UserFunc fargs stmts retExpr) <- case value name fs of
-        Left _ -> Left $ ValueError ("No such function: " ++ name)
+        Left _ -> Left $ ValueError ("Function " ++ name ++ " doesn not exist")
         Right f -> Right f
     if length args == length fargs
        then do evaledArgs <- sequence $ map (eval vars fs) args
@@ -165,6 +171,8 @@ eval vars fs (LessThan a b) = do
     bV <- eval vars fs b
     case (aV, bV) of
         (IntVal left, IntVal right) -> Right $ BoolVal $ left < right
+        (FloatVal left, IntVal right) -> Right $ BoolVal $ left < (fromIntegral right)
+        (IntVal left, FloatVal right) -> Right $ BoolVal $ (fromIntegral left) < right
         (FloatVal left, FloatVal right) -> Right $ BoolVal $ left < right
         _ -> Left $ ValueError "Uncomparable types"
 
@@ -173,6 +181,8 @@ eval vars fs (GreaterThan a b) = do
     bV <- eval vars fs b
     case (aV, bV) of
         (IntVal left, IntVal right) -> Right $ BoolVal $ left > right
+        (FloatVal left, IntVal right) -> Right $ BoolVal $ left > (fromIntegral right)
+        (IntVal left, FloatVal right) -> Right $ BoolVal $ (fromIntegral left) > right
         (FloatVal left, FloatVal right) -> Right $ BoolVal $ left > right
         _ -> Left $ ValueError "Uncomparable types"
 
@@ -182,6 +192,8 @@ checkExprEq a b = do
     case (a, b) of
         (StrVal left, StrVal right) -> left == right
         (IntVal left, IntVal right) -> left == right
+        (FloatVal left, IntVal right) -> left == (fromIntegral right)
+        (IntVal left, FloatVal right) -> (fromIntegral left) == right
         (FloatVal left, FloatVal right) -> left == right
         (BoolVal left, BoolVal right) -> left == right
         _ -> False
