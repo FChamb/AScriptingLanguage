b4aabc0054ff769fc487068fe6f73a7035d97fdd
st275 <st275@st-andrews.ac.uk>
2024-03-23 09:38:06 +0000

    Comments in Error.hs

--- Code/Error.hs
+++ Code/Error.hs
@@ -1,10 +1,14 @@
 module Error where
 
-data Error = ParseError String | MathError String | ValueError String | TreeError String
+-- data type for errors in this scripting language
+data Error = ParseError String -- Use for problems with Parser
+            | MathError String -- Use for incorrect mathematics
+            | ValueError String -- Use for incorrect data types
+            | TreeError String -- Use with BinaryTree.hs
     deriving (Eq)
 
 instance Show Error where
-    show (ParseError i) = "ParseError: " ++ i
+    show (ParseError i) = "ParseError: " ++ i 
     show (MathError f) =  "MathError: " ++ f
     show (ValueError s) = "ValueError: " ++ s
     show (TreeError t) = "TreeError: " ++ t
