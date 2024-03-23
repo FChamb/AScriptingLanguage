39e9d956939bfc49fc09d36e45511483141e838c
st275 <st275@st-andrews.ac.uk>
2024-03-23 10:04:51 +0000

    Clean Expr.hs comments + org

--- Code/Expr.hs
+++ Code/Expr.hs
@@ -4,7 +4,7 @@ import BinaryTree
 import Control.Monad.State
 import Error
 
-type Name = String
+type Name = String -- type definition for variable names
 
 data Value = IntVal Int | FloatVal Float | StrVal String | BoolVal Bool
     deriving (Eq)
@@ -15,28 +15,10 @@ instance Show Value where
     show (StrVal s) = s
     show (BoolVal b) = show b
 
-type Vars = Tree (Name, Value)
+type Vars = Tree (Name, Value) -- variables represented as binary tree
 
-{-
- - Functions
- -
- - Functions work by having a series a statements (terminted by a semi colon)
- - followed by a final "return" expression, which is evaluated and given
- - as the result of the function as a whole
- -}
-data UserFunc = UserFunc [Name] [FuncStatement] Expr
-    deriving (Show)
-data FuncStatement = FuncSetVar Name Expr -- TODO: support some kind of if too?
-    deriving (Show)
-type Funcs = Tree (Name, UserFunc)
-
-data Env = Env {vars :: Vars, funcs :: Funcs, history :: [Command]}
-type Eval a = StateT Env (Either Error) a
---data State = State {vars :: Tree (Name, Value), funcs :: Funcs, history :: [Command]}
-
--- Given a variable name and a value, return a new set of variables with
--- that name and value added.
--- If it already exists, remove the old value
+{- Given a variable name and a value, updates state with that name 
+ - and value added. If it already exists, overwrites old value -}
 updateVars :: Name -> Value -> Eval ()
 updateVars name val = do
     env <- get
@@ -58,7 +40,7 @@ data Expr = Add Expr Expr
           | ToFloat Expr
           | Concat Expr Expr
           | CallUserFunc Name [Expr] -- User defined function, with variable number of arguments
-          | IsEq Expr Expr -- Boolean operations
+          | IsEq Expr Expr           -- Boolean operations
           | NotEq Expr Expr
           | LessThan Expr Expr
           | GreaterThan Expr Expr
@@ -74,9 +56,26 @@ data Command = Set Name Expr             -- assign an expression to a variable n
              | Repeat Int [Command]      -- Prompt for repeating a command
              | Block [Command]           -- Representing a block command
              | DefUserFunc Name UserFunc -- Define a function
-             | If Expr Command Command   -- Define if then else
-             | While Expr Command        -- Prompt for While loop
-             | For Expr Command Command  -- Prompt for For loop
+             | If Expr Command Command   -- If then else construct
+             | While Expr Command        -- While loop construct
+             | For Expr Command Command  -- For loop construct
              | Quit                      -- Prompt for quiting program
              | Help                      -- Prompt for showing helpful options
   deriving Show
+
+{-
+ - Functions
+ -
+ - Functions work by having a series a statements (terminted by a semi colon)
+ - followed by a final "return" expression, which is evaluated and given
+ - as the result of the function as a whole
+ -}
+data UserFunc = UserFunc [Name] [FuncStatement] Expr
+    deriving (Show)
+data FuncStatement = FuncSetVar Name Expr -- TODO: support some kind of if too?
+    deriving (Show)
+type Funcs = Tree (Name, UserFunc)
+
+data Env = Env {vars :: Vars, funcs :: Funcs, history :: [Command]}
+type Eval a = StateT Env (Either Error) a
+--data State = State {vars :: Tree (Name, Value), funcs :: Funcs, history :: [Command]}
